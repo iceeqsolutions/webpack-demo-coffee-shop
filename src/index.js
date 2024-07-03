4fd6183cfecadacbb276b0f30d678720d6ddd284
iCeeqSolutions <155289437+iceeqsolutions@users.noreply.github.com>
2024-07-03 17:58:12 +0300

    ADD footnote to start page via separate module
    - Add new module to project (icsFootnote.js)
    - Add link to iCS-homepage via the iCS-logo (open on new tab)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles/styles.css";
 import home from "./modules/home";
+import icsStatement from "./modules/icsFootnote";
 
 const content = document.querySelector("#content");
 content.appendChild(home());
+content.appendChild(icsStatement());
