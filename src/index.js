1d29c4ab228dee3477bbb63c873ab07393a693fa
iCeeqSolutions <155289437+iceeqsolutions@users.noreply.github.com>
2024-07-07 18:57:02 +0300

    ADD new location page module to project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles/styles.css";
 import home from "./modules/home";
 import about from "./modules/about";
 import menu from "./modules/menu";
+import location from "./modules/location";
 import contact from "./modules/contact";
 import icsStatement from "./modules/icsFootnote";
 
@@ -9,17 +10,23 @@ import icsStatement from "./modules/icsFootnote";
 const homePage = document.querySelector(".home");
 const aboutPage = document.querySelector(".about");
 const menuPage = document.querySelector(".menu");
+const locationPage = document.querySelector(".location");
 const contactPage = document.querySelector(".contact");
 
 const content = document.querySelector("#content");
 content.appendChild(home());
 homePage.classList.add("active");
+aboutPage.classList.remove("active");
+menuPage.classList.remove("active");
+contactPage.classList.remove("active");
+locationPage.classList.remove("active");
 content.appendChild(icsStatement());
 
 // Navigation
 homePage.addEventListener("click", goToHome);
 aboutPage.addEventListener("click", goToAbout);
 menuPage.addEventListener("click", goToMenu);
+locationPage.addEventListener("click", goToLocation);
 contactPage.addEventListener("click", goToContact);
 
 function goToHome() {
@@ -31,41 +38,58 @@ function goToHome() {
     aboutPage.classList.remove("active");
     menuPage.classList.remove("active");
     contactPage.classList.remove("active");
+    locationPage.classList.remove("active");
     content.appendChild(icsStatement());
-}
-
-function goToAbout() {
+  }
+  
+  function goToAbout() {
     while (content.firstChild) {
-        content.removeChild(content.firstChild);
+      content.removeChild(content.firstChild);
     }
     content.appendChild(about());
     homePage.classList.remove("active");
     aboutPage.classList.add("active");
     menuPage.classList.remove("active");
     contactPage.classList.remove("active");
+    locationPage.classList.remove("active");
     content.appendChild(icsStatement());
-}
-
-function goToMenu() {
+  }
+  
+  function goToMenu() {
     while (content.firstChild) {
-        content.removeChild(content.firstChild);
+      content.removeChild(content.firstChild);
     }
     content.appendChild(menu());
     homePage.classList.remove("active");
     aboutPage.classList.remove("active");
     menuPage.classList.add("active");
     contactPage.classList.remove("active");
+    locationPage.classList.remove("active");
     content.appendChild(icsStatement());
-}
-
-function goToContact() {
+  }
+  
+  function goToLocation() {
     while (content.firstChild) {
-        content.removeChild(content.firstChild);
+      content.removeChild(content.firstChild);
+    }
+    content.appendChild(location());
+    homePage.classList.remove("active");
+    aboutPage.classList.remove("active");
+    menuPage.classList.remove("active");
+    contactPage.classList.remove("active");
+    locationPage.classList.add("active");
+    content.appendChild(icsStatement());
+  }
+    
+  function goToContact() {
+    while (content.firstChild) {
+      content.removeChild(content.firstChild);
     }
     content.appendChild(contact());
     homePage.classList.remove("active");
     aboutPage.classList.remove("active");
     menuPage.classList.remove("active");
     contactPage.classList.add("active");
+    locationPage.classList.remove("active");
     content.appendChild(icsStatement());
-}
+  }
---
+++ src/modules/location.js
@@ -0,0 +1,8 @@
+function location() {
+    const container = document.createElement("div");
+    container.classList.add("locationPage");
+
+    return container;
+}
+
+export default location;
