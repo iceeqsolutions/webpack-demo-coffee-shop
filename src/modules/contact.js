023708c6c412adae9f0fff32535fb951fb9ecafb
iCeeqSolutions <155289437+iceeqsolutions@users.noreply.github.com>
2024-07-08 10:27:37 +0300

    ADD content to contact page
    - Add contact form to page using the custom createFormElement method

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,6 +1,54 @@
+import createElement from "./createFormElement";
+
 function contact() {
     const container = document.createElement("div");
-    container.classList = "contactPage";
+  const form = createElement("form", container, {
+    class: "contactForm",
+  });
+  const h3 = createElement(
+    "h3",
+    form,
+    {
+      class: "formHeading formField",
+    },
+    "Contact Us / Give Us Feedback"
+  );
+  const firstName = createElement("input", form, {
+    type: "text",
+    placeholder: "First Name",
+    class: "firstNameInput formField",
+    required: true,
+  });
+  const lastName = createElement("input", form, {
+    type: "text",
+    placeholder: "Last name",
+    class: "lastNameInput formField",
+  });
+  const phone = createElement("input", form, {
+    type: "tel",
+    placeholder: "Phone number",
+    class: "phoneInput formField",
+  });
+  const email = createElement("input", form, {
+    type: "email",
+    placeholder: "Email",
+    class: "emailInput formField",
+    required: true,
+  });
+  const subject = createElement("textarea", form, {
+    type: "text",
+    placeholder: "Message",
+    class: "subjectInput formField",
+    required: true,
+  });
+  const submit = createElement(
+    "button",
+    form,
+    { type: "submit", class: "submitForm" },
+    "Send"
+  );
+  container.className = "contactPage";
+  container.classList.add("activePage");
 
     return container;
 }
