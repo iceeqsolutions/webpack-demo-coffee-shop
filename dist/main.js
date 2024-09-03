e82047f19bf7f1d89d45d63409d4508622c4e6cc
iCeeqSolutions <155289437+iceeqsolutions@users.noreply.github.com>
2024-09-03 18:00:13 +0300

    MOD iCeeqSolutions logo in README.md file

--- dist/main.js
+++ dist/main.js
@@ -568,7 +568,7 @@ eval("module.exports = __webpack_require__.p + \"c115fba26d042f0312aa.jpg\";\n\n
 /******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
 /******/ 		var document = __webpack_require__.g.document;
 /******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
+/******/ 			if (document.currentScript && document.currentScript.tagName.toUpperCase() === 'SCRIPT')
 /******/ 				scriptUrl = document.currentScript.src;
 /******/ 			if (!scriptUrl) {
 /******/ 				var scripts = document.getElementsByTagName("script");
